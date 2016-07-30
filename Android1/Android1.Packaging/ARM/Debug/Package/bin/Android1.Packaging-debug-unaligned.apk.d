fd228a5de4f8f3c03779325a7b252b2d4274a259
nativeAndroid <publicator@yandex.ru>
2016-07-31 00:01:57 +0300

    order

--- Android1/Android1.Packaging/ARM/Debug/Package/bin/Android1.Packaging-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-C:\Users\Happy\documents\visual studio 2015\Projects\Android2\Android1\Android1.Packaging\ARM\Debug\Package\bin\Android1.Packaging-debug-unaligned.apk : \
-C:\Users\Happy\documents\visual studio 2015\Projects\Android2\Android1\Android1.Packaging\ARM\Debug\Package\bin\Android1.Packaging.ap_ \
-C:\Users\Happy\documents\visual studio 2015\Projects\Android2\Android1\Android1.Packaging\ARM\Debug\Package\libs\armeabi-v7a\gdbserver \
-C:\Users\Happy\documents\visual studio 2015\Projects\Android2\Android1\Android1.Packaging\ARM\Debug\Package\libs\armeabi-v7a\libAndroid1.so \
--- Android1/Android1.Packaging/ARM/Debug/Package/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\Users\Happy\documents\visual studio 2015\Projects\Android2\Android1\Android1.Packaging\ARM\Debug\Package\bin\AndroidManifest.xml : \
-C:\Users\Happy\documents\visual studio 2015\Projects\Android2\Android1\Android1.Packaging\ARM\Debug\Package\AndroidManifest.xml \
